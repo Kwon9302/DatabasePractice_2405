5b24002a4431de1dd9dff93f462eed680d7220b6
Kwon9302 <148173045+Kwon9302@users.noreply.github.com>
2024-05-23 22:46:54 +0900

    Relationship between DB, Entity

--- db/src/main/java/apidb/saveDb/BikeDB.java
+++ db/src/main/java/apidb/saveDb/BikeDB.java
@@ -21,6 +21,11 @@ public class BikeDB {
     private int shared;
 //    private double stationLatitude;
 //    private double stationLongitude;
-//    private String stationId;
-
+// //    private String stationId;
+// Entity에서 선언한 값들을 기준으로 save된다.
+// => a:1, b:2, c:3의 json 데이터를 불러오고 save할때 entity에 
+// private int a;
+// private int b;
+// 만 선언되어있다면 a,b의 값만 읽어온다.
+// But, sql column은 생성되어 있어야 한다. (자동생성x)
 }
